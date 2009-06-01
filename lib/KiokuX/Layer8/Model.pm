7af6bb9ebeab4869f295a3f98fe379977cd7af04
Chris Prather <cprather@hdpublishing.com>
2009-06-01 11:18:27 -0400

    remove Params::Validate

--- lib/KiokuX/Layer8/Model.pm
+++ lib/KiokuX/Layer8/Model.pm
@@ -1,17 +1,11 @@
 package KiokuX::Layer8::Model;
 use Moose::Role;
-use MooseX::Params::Validate;
-
 use namespace::autoclean;
 
 requires qw(insert search);
 
 sub add_user {
-    my $self = shift;
-    my ($user) =
-      pos_validated_list( \@_,
-        { isa => 'KiokuX::Layer8::User', coerce => 1, } );
-
+    my ( $self, $user ) = @_;
     $self->txn_do( sub { $self->insert($user) } );
     return $user;
 }
